3d54c2f287317cbd98bc9951bf02735fc6df46c9
Stewart MacLeman <stewart_macleman@hotmail.com>
2021-08-06 11:10:32 +0100

    Basic layout styles.

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
 "use strict";
 
-console.log("hello!");
+let contentArray = [
+  {
+    h3Title: "The Markdown Subject Title",
+    paragraphContent: "This is a Paragraph.",
+  },
+  {
+    h3Title: "The Markdown Subject Title",
+    paragraphContent: "This is a Paragraph.",
+  },
+  {
+    h3Title: "The Markdown Subject Title",
+    paragraphContent: "This is a Paragraph.",
+  },
+  {
+    h3Title: "The Markdown Subject Title",
+    paragraphContent: "This is a Paragraph.",
+  }
+];
+
+// Global variables. ----------------------------------------
+let displayContentButton = document.querySelectorAll(".showButton");
+
+
+// Show the content of each topic. ---------------------------
+for (let i = 0; i < displayContentButton.length; i++){
+  displayContentButton[i].addEventListener("click", displayContent);
+}
+//
+function displayContent(e){
+  let prevElement = e.target.previousElementSibling;
+  prevElement.classList.toggle("hide");
+  // console.log("Hello from the button!");
+}
